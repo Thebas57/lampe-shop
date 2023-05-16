43770ed482b25dcdad81b0d3f7cac4671d4a5bb1
Thebas57 <theohelf@gmail.com>
2023-05-16 14:01:34 +0200

    début du projet / navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <h1>ease</h1>
+        <ul>
+            <li>Collections</li>
+            <li>Blog</li>
+            <li>About</li>
+            <li>Contacts</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
