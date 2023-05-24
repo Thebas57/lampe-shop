3b6ff892e9af08fc8accbc48fcfd9f9fa9d69fb5
Thebas57 <theohelf@gmail.com>
2023-05-24 11:53:42 +0200

    html container

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="container">
-        <div className="left"></div>
+        <div className="left">
+          <p>
+            Collection of lighting is inspired by the geometric works of the
+            great Suprematist artists Kissitzky and Kazimir Malevich.
+            <br /><br />
+            Suprematism is a modernist movement in the art of the early
+            twentieth century, focused on the basic geometric forms, such as
+            circles, squares, lines and rectangles. The geometric structure of
+            the lamps will always look like a small art objects in your house.
+          </p>
+        </div>
         <div className="right"></div>
       </div>
     </div>
